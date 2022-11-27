a36d7a2cc7ac6bfa1bd2d751c3adc263fe9356c6
MyBlackMIDIScore <mbms@xheonin.com>
2022-11-27 14:15:44 +0200

    Improve keyboard model

--- src/gui/window/keyboard.rs
+++ src/gui/window/keyboard.rs
@@ -4,6 +4,11 @@ use crate::midi::MIDIColor;
 
 use super::keyboard_layout::KeyboardView;
 
+enum GradDirection {
+    Up,
+    Down,
+}
+
 pub struct GuiKeyboard {}
 
 impl GuiKeyboard {
@@ -20,14 +25,15 @@ impl GuiKeyboard {
     ) {
         let (rect, _) = ui.allocate_exact_size(ui.available_size(), Sense::click());
         let mut mesh = Mesh::default();
-        let key_density =
-            (((rect.width() as usize / key_view.visible_range.len()) / 8) as u8).max(1);
-        let onepx = ui.painter().round_to_pixel(1.0 * key_density as f32);
+        let key_density = ((rect.width() / key_view.visible_range.len() as f32) / 8.0)
+            .max(1.0)
+            .min(5.0);
+        let onepx = ui.painter().round_to_pixel(key_density);
 
-        let md_height = rect.height() * 0.042;
-        let bar = rect.height() * 0.05;
+        let md_height = rect.height() * 0.044;
+        let bar = rect.height() * 0.06;
 
-        let black_key_overlap = md_height / 1.5;
+        let black_key_overlap = bar / 2.0;
         let top = rect.top() + bar;
         let bottom = rect.bottom();
         let black_bottom = rect.bottom() - rect.height() * 0.34;
@@ -51,7 +57,7 @@ impl GuiKeyboard {
                     let rect2 = Rect::from_min_max(top_left2, bottom_right2);
                     let color2 = Color32::from_rgb(color.r() / 2, color.g() / 2, color.b() / 2);
 
-                    mesh.add_colored_rect(rect, color);
+                    draw_gradient_darken(&mut mesh, rect, color, GradDirection::Up, 0.5);
                     mesh.add_colored_rect(rect2, color2);
                 } else {
                     // Not pressed
@@ -63,25 +69,25 @@ impl GuiKeyboard {
                     let bottom_right1 = Pos2::new(map_x(key.right), bottom - white_key_bottom);
                     let rect1 = Rect::from_min_max(top_left1, bottom_right1);
 
-                    // Bottom line separator
+                    // Bottom part
                     let top_left2 = Pos2::new(map_x(key.left), bottom - white_key_bottom);
                     let bottom_right2 = Pos2::new(map_x(key.right), bottom);
                     let rect2 = Rect::from_min_max(top_left2, bottom_right2);
-                    let color2 = Color32::from_rgb(150, 150, 150);
+                    let color2 = Color32::from_rgb(190, 190, 190);
 
-                    // Bottom part
+                    // Bottom line separator
                     let top_left3 = Pos2::new(map_x(key.left), bottom - white_key_bottom);
                     let bottom_right3 =
                         Pos2::new(map_x(key.right), bottom - white_key_bottom + onepx);
                     let rect3 = Rect::from_min_max(top_left3, bottom_right3);
                     let color3 = Color32::from_rgb(100, 100, 100);
 
-                    mesh.add_colored_rect(rect1, color);
-                    mesh.add_colored_rect(rect2, color2);
+                    draw_gradient_darken(&mut mesh, rect1, color, GradDirection::Up, 0.6);
+                    draw_gradient_darken(&mut mesh, rect2, color2, GradDirection::Down, 0.3);
                     mesh.add_colored_rect(rect3, color3);
                 }
                 // White key borders
-                let color4 = Color32::from_rgb(20, 20, 20);
+                let color4 = Color32::from_rgb(45, 45, 45);
                 let top_left4 = Pos2::new(map_x(key.right), top);
                 let bottom_right4 = Pos2::new(map_x(key.right) - onepx, bottom);
                 let rect4 = Rect::from_min_max(top_left4, bottom_right4);
@@ -90,10 +96,15 @@ impl GuiKeyboard {
         }
 
         let bar_top_left = Pos2::new(rect.left(), rect.top());
-        let bar_bottom_right = Pos2::new(rect.right(), top);
+        let bar_bottom_right = Pos2::new(rect.right(), top - onepx);
         let bar_rect = Rect::from_min_max(bar_top_left, bar_bottom_right);
+        draw_gradient_darken(&mut mesh, bar_rect, *bar_color, GradDirection::Up, 0.3);
 
-        mesh.add_colored_rect(bar_rect, *bar_color);
+        let sepr_top_left = Pos2::new(rect.left(), top - onepx);
+        let sepr_bottom_right = Pos2::new(rect.right(), top);
+        let sepr_rect = Rect::from_min_max(sepr_top_left, sepr_bottom_right);
+        let sepr_color = Color32::from_rgb(60, 60, 60);
+        mesh.add_colored_rect(sepr_rect, sepr_color);
 
         for (i, key) in key_view.iter_visible_keys() {
             if key.black {
@@ -103,15 +114,15 @@ impl GuiKeyboard {
                     let top_left1 = Pos2::new(map_x(key.left), top);
                     let bottom_right1 = Pos2::new(map_x(key.right), black_bottom);
                     let rect1 = Rect::from_min_max(top_left1, bottom_right1);
-                    let color1 = Color32::from_rgb(color.r() / 2, color.g() / 2, color.b() / 2);
+                    let color1 = Color32::from_rgb(color.r() / 3, color.g() / 3, color.b() / 3);
 
                     // Surface
-                    let top_left2 = Pos2::new(map_x(key.left) + onepx, top);
+                    let top_left2 = Pos2::new(map_x(key.left) + onepx, top - onepx);
                     let bottom_right2 = Pos2::new(map_x(key.right) - onepx, black_bottom - onepx);
                     let rect2 = Rect::from_min_max(top_left2, bottom_right2);
 
                     mesh.add_colored_rect(rect1, color1);
-                    mesh.add_colored_rect(rect2, color);
+                    draw_gradient_darken(&mut mesh, rect2, color, GradDirection::Down, 0.55);
                 } else {
                     // Not pressed
                     let black_key_bottom = md_height;
@@ -120,21 +131,46 @@ impl GuiKeyboard {
                     let top_left1 = Pos2::new(map_x(key.left) - onepx, top);
                     let bottom_right1 = Pos2::new(map_x(key.right) + onepx, black_bottom);
                     let rect1 = Rect::from_min_max(top_left1, bottom_right1);
-                    let color1 = Color32::from_rgb(62, 62, 62);
+                    let color1 = Color32::from_rgb(65, 65, 65);
 
                     // Surface
                     let top_left2 = Pos2::new(map_x(key.left) + onepx, top - black_key_overlap);
                     let bottom_right2 =
                         Pos2::new(map_x(key.right) - onepx, black_bottom - black_key_bottom);
                     let rect2 = Rect::from_min_max(top_left2, bottom_right2);
-                    let color2 = Color32::from_rgb(24, 24, 24);
+                    let color2 = Color32::from_rgb(38, 38, 38);
 
-                    mesh.add_colored_rect(rect1, color1);
-                    mesh.add_colored_rect(rect2, color2);
+                    draw_gradient_darken(&mut mesh, rect1, color1, GradDirection::Up, 0.55);
+                    draw_gradient_darken(&mut mesh, rect2, color2, GradDirection::Down, 0.3);
                 }
             }
         }
 
         ui.painter().add(mesh);
+    }
+}
+
+fn draw_gradient_darken(mesh: &mut Mesh, rect: Rect, color: Color32, direction: GradDirection, mult: f32) {
+    let idx = mesh.vertices.len() as u32;
+    mesh.add_triangle(idx + 0, idx + 1, idx + 2);
+    mesh.add_triangle(idx + 2, idx + 1, idx + 3);
+    let darken = Color32::from_rgb(
+        (color.r() as f32 * mult) as u8,
+        (color.g() as f32 * mult) as u8,
+        (color.b() as f32 * mult) as u8,
+    );
+    match direction{
+        GradDirection::Up => {
+            mesh.colored_vertex(rect.left_top(), darken);
+            mesh.colored_vertex(rect.right_top(), darken);
+            mesh.colored_vertex(rect.left_bottom(), color);
+            mesh.colored_vertex(rect.right_bottom(), color);
+        }
+        GradDirection::Down => {
+            mesh.colored_vertex(rect.left_top(), color);
+            mesh.colored_vertex(rect.right_top(), color);
+            mesh.colored_vertex(rect.left_bottom(), darken);
+            mesh.colored_vertex(rect.right_bottom(), darken);
+        }
     }
 }
