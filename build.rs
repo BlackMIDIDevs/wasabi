04eaeb16fbbade76c15fae43dc66c1edef1211a5
Isaac Mills <rooster0055@protonmail.com>
2023-04-17 20:10:44 -0400

    Add windows icon compatibility

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
-use std::path::Path;
+use std::{fs::File, path::Path};
 
+use ico::IconDirEntry;
 use resvg::{
     tiny_skia::{Pixmap, Transform},
     usvg::{Options, Tree, TreeParsing},
 };
+use winres::WindowsResource;
 
 fn main() {
     let svg = std::fs::read_to_string("logo.svg").unwrap();
@@ -25,6 +27,34 @@ fn main() {
         pixmap.data(),
     )
     .unwrap();
+    pixmap
+        .save_png(Path::new(std::env::var_os("OUT_DIR").as_ref().unwrap()).join("logo_16x16.png"))
+        .unwrap();
 
-    println!("cargo:rerun-if-changed=logo.svg");
+    let mut icon_dir = ico::IconDir::new(ico::ResourceType::Icon);
+
+    let image = ico::IconImage::from_rgba_data(16, 16, pixmap.take());
+    icon_dir.add_entry(IconDirEntry::encode(&image).unwrap());
+
+    for s in [24, 32, 48, 128, 256] {
+        let mut pixmap = Pixmap::new(s, s).unwrap();
+
+        resvg::render(
+            &tree,
+            resvg::FitTo::Size(s, s),
+            Transform::default(),
+            pixmap.as_mut(),
+        )
+        .unwrap();
+
+        let image = ico::IconImage::from_rgba_data(s, s, pixmap.take());
+        icon_dir.add_entry(IconDirEntry::encode(&image).unwrap());
+    }
+    let icon_path = Path::new(std::env::var_os("OUT_DIR").as_ref().unwrap()).join("icon.ico");
+    icon_dir.write(File::create(&icon_path).unwrap()).unwrap();
+
+    #[cfg(windows)]
+    {
+        WindowsResource::new().set_icon(icon_path.to_str().unwrap());
+    }
 }
